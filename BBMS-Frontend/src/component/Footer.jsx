e3c508eda20fcdc3ee3042c475bd4776aa4eff5b
Janitha <janithasandaruwan97@gmail.com>
2024-09-08 11:31:21 +0530

    login page success and donor register some parts are done

---
+++ BBMS-Frontend/src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="py-4 text-white bg-gray-800">
+            <div className="container mx-auto text-center">
+                <p>&copy; 2022 Your Company. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
